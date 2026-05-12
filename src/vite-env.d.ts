672b51ee9112e841fdfe008eceb7e6bec5ffb32c
chyopriushy <mmkanggg@gmail.com>
2026-05-12 14:52:56 +0900

    refactor: replace Kakao SDK with OAuth redirect flow

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,8 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
-  readonly VITE_KAKAO_JS_KEY: string;
-  readonly VITE_KAKAO_REST_KEY: string;
+  readonly VITE_KAKAO_CLIENT_ID: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
   readonly VITE_NAVER_CLIENT_ID: string;
   readonly VITE_NAVER_REDIRECT_URI: string;
   readonly VITE_GOOGLE_CLIENT_ID: string;
