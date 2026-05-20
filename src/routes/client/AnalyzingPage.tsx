21eb8b52d30e5feb133374466098123ade00ee92
이총명 <dlchdaud04@naver.com>
2026-05-20 13:31:33 +0900

    save

--- src/routes/client/AnalyzingPage.tsx
+++ src/routes/client/AnalyzingPage.tsx
@@ -239,7 +239,7 @@ export function AnalyzingPage() {
               {/* Center shield-check */}
               <img src={shieldCheck} alt="" className="relative z-10 h-4 w-4" />
               {/* Small bottom-right circle */}
-              <div className="absolute right-[8px] top-[32px] h-8 w-8 rounded-2xl border-2 border-white shadow-[0px_4px_6px_0px_rgba(0,0,0,0.1)]" />
+          
             </div>
 
             {/* Title */}
